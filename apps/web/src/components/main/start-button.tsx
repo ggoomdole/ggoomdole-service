9530a0ab02c023338ed5841fff143117d4386b54
gs0428 <lider28@naver.com>
2025-07-01 22:30:45 +0900

    feat: 첫 화면 구현

---
+++ apps/web/src/components/main/start-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import KakaoIcon from "@/assets/kakao.svg";
+
+export default function StartButton() {
+  const router = useRouter();
+
+  const onRouteOnboarding = () => {
+    router.push("/onboarding");
+  };
+
+  const onRouteHome = () => {
+    router.push("/home");
+  };
+
+  return (
+    <section className="mb-24 flex flex-col items-center">
+      <button className="text-regular mb-4 text-gray-700 underline" onClick={onRouteOnboarding}>
+        시작하기에 앞서
+      </button>
+      <div className="text-medium space-y-2">
+        <button className="bg-kakao flex w-[90vw] items-center justify-center gap-2 rounded-xl py-2">
+          <KakaoIcon className="size-4" />
+          <p>카카오 로그인</p>
+        </button>
+        <button
+          className="border-kakao w-[90vw] rounded-xl border bg-white py-2 text-center"
+          onClick={onRouteHome}
+        >
+          로그인 없이 둘러보기
+        </button>
+      </div>
+    </section>
+  );
+}
--- apps/web/src/page/index.tsx
+++ apps/web/src/page/index.tsx
@@ -1,3 +1,25 @@
+import Image from "next/image";
+
+import StartButton from "@/components/main/start-button";
+
+const miniGgoomdole = "/static/onboarding/mini-ggoomdole.png";
+const logo = "/static/logo.png";
+
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <main className="flex flex-col">
+      <section className="flex flex-1 flex-col items-center justify-center">
+        <Image
+          src={miniGgoomdole}
+          alt="미니 꿈돌이"
+          width={100}
+          height={132}
+          className="translate-y-5"
+        />
+        <Image src={logo} alt="순례해유 로고" width={248} height={60} className="z-10" />
+        <p className="text-regular mt-2.5">나만의 순례길을 만들어봐요!!</p>
+      </section>
+      <StartButton />
+    </main>
+  );
 }
