41003f3b27c70093aa82935cef27a7d82e86b68a
gs0428 <lider28@naver.com>
2025-07-01 21:27:54 +0900

    feat: 공통 헤더 컴포넌트 구현

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import ArrowLeft from "@/assets/arrow-left.svg";
+
+const logo = "/static/logo.png";
+
+interface CommonHeaderProps {
+  logoHeader?: boolean;
+  rightElement?: React.ReactNode;
+  children: React.ReactNode;
+  onClickLeft?: () => void;
+}
+
+export default function CommonHeader({
+  logoHeader,
+  rightElement,
+  children,
+  onClickLeft,
+}: CommonHeaderProps) {
+  const router = useRouter();
+
+  const onClickLeftButton = () => {
+    if (onClickLeft) {
+      return onClickLeft();
+    }
+    router.back();
+  };
+
+  return (
+    <header className="h-header shadow-layout z-50 flex items-center justify-between gap-2.5 bg-white px-5">
+      {logoHeader ? (
+        <Image src={logo} alt="순례해유 로고" width={100} height={24} />
+      ) : (
+        <button onClick={onClickLeftButton}>
+          <ArrowLeft />
+        </button>
+      )}
+      {!logoHeader && <div className="text-semibold flex-1 text-center">{children}</div>}
+      <div className="flex min-w-6 items-center">{rightElement}</div>
+    </header>
+  );
+}
