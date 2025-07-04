ddd9cd1c83ae6cb22f1121ededc67fc191d1cb0d
Kim Minji <128395453+songwol38@users.noreply.github.com>
2025-07-04 15:51:24 +0900

    [FB](chore) 백엔드 초기 세팅 (#13)
    프리즈마, 도커, 에러처리

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,17 +1,23 @@
 import cors from "cors";
 import express from "express";
 
+import { errorHandler } from './middlewares/errorHandler';
+
 const app = express();
+
 app
   .use(cors())
   .use(express.urlencoded({ extended: true }))
   .use(express.json())
+
   .get("/", (_, res) => {
-    return res.json({ message: "Hello World" });
-  });
+    return res.json({ message: "서버 실행 완료" });
+  })
+
+  .use(errorHandler);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`api running on ${port}`);
+  console.log(`백엔드 서버 ${port}포트`);
 });
