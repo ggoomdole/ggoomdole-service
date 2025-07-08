1552a2a22eec1168f02e140ae4fb6736ee4e448e
KIMMINJI <amber_lucy@naver.com>
2025-07-08 13:42:46 +0900

    feat : dto 설정

--- packages/types/src/dto.ts
+++ packages/types/src/dto.ts
@@ -1,3 +1,12 @@
-export interface dto {
-    
+export interface AddLoadDTO {
+    title: string;
+    intro: string;
+    categoryId: number;
+    spots: SpotDTO[] | string;
+}
+
+export interface SpotDTO {
+    spotId: number;
+    number: number;
+    introSpot: string;
 }
