cdbea5227869405f47aa59a4403dbd1be5d255e5
gs0428 <lider28@naver.com>
2025-08-24 18:49:10 +0900

    fix: 빌드 오류 해결

--- apps/web/src/schemas/course.ts
+++ apps/web/src/schemas/course.ts
@@ -1,15 +1,16 @@
 import { z } from "zod";
 
+const courseSchema = z.object({
+  placeName: z.string().min(1),
+  reason: z.string().min(1),
+  placeId: z.string().min(1),
+  address: z.string().min(1),
+  latitude: z.number(),
+  longitude: z.number(),
+});
+
 export const requestCourseFormSchema = z.object({
-  places: z
-    .array(
-      z.object({
-        placeName: z.string().min(1),
-        reason: z.string().min(1),
-        placeId: z.string().min(1),
-      })
-    )
-    .min(1),
+  places: z.array(courseSchema).min(1),
 });
 
 export type RequestCourseForm = z.infer<typeof requestCourseFormSchema>;
@@ -19,18 +20,7 @@ export const uploadCourseFormSchema = z.object({
   thumbnail: z.instanceof(File),
   category: z.string().min(1),
   intro: z.string().min(1),
-  spots: z
-    .array(
-      z.object({
-        placeName: z.string().min(1),
-        reason: z.string().min(1),
-        placeId: z.string().min(1),
-        address: z.string().min(1),
-        latitude: z.number(),
-        longitude: z.number(),
-      })
-    )
-    .min(1),
+  spots: z.array(courseSchema).min(1),
   removeCourseIds: z.array(z.string()).optional(),
 });
 
