33001ad854fbc975610bccaa97f6cb593764c003
gs0428 <lider28@naver.com>
2025-09-14 16:18:38 +0900

    fix: body 용량 제한 해제

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,31 +1,32 @@
 import cors from "cors";
 import express from "express";
 
-import { errorHandler } from './middlewares/errorHandler';
-import authRoute from './routes/authRoute';
-import reviewRoute from './routes/reviewRoute';
-import roadRoute from './routes/roadRoute';
-import searchRoute from './routes/searchRoute';
-import spotRoute from './routes/spotRoute';
-import userRoute from './routes/userRoute';
+import { errorHandler } from "./middlewares/errorHandler";
+import authRoute from "./routes/authRoute";
+import reviewRoute from "./routes/reviewRoute";
+import roadRoute from "./routes/roadRoute";
+import searchRoute from "./routes/searchRoute";
+import spotRoute from "./routes/spotRoute";
+import userRoute from "./routes/userRoute";
 
 const app = express();
 
 app
   .use(cors())
   .use(express.urlencoded({ extended: true }))
-  .use(express.json())
+  .use(express.json({ limit: "20mb" }))
+  .use(express.urlencoded({ limit: "20mb", extended: true }))
 
   .get("/", (_, res) => {
     return res.json({ message: "서버 실행 완료" });
   })
 
-  .use('/login', authRoute)
-  .use('/users', userRoute)
-  .use('/road', roadRoute)
-  .use('/search', searchRoute)
-  .use('/review', reviewRoute)
-  .use('/spot', spotRoute)
+  .use("/login", authRoute)
+  .use("/users", userRoute)
+  .use("/road", roadRoute)
+  .use("/search", searchRoute)
+  .use("/review", reviewRoute)
+  .use("/spot", spotRoute)
 
   .use(errorHandler);
 
