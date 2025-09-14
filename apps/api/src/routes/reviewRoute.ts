76494d131a220390f51a5e005363fec1e69021fb
gs0428 <lider28@naver.com>
2025-09-14 16:12:29 +0900

    fix: 파일 용량 제한 명시

--- apps/api/src/routes/reviewRoute.ts
+++ apps/api/src/routes/reviewRoute.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import multer from 'multer';
+import { Router } from "express";
+import multer from "multer";
 
-import reviewController from '../controllers/reviewController';
-import authenticate from '../middlewares/authenticate';
+import reviewController from "../controllers/reviewController";
+import authenticate from "../middlewares/authenticate";
 
 const router = Router();
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } });
 
-router.post("/", authenticate, upload.single('review-image'), reviewController.createReview);
+router.post("/", authenticate, upload.single("review-image"), reviewController.createReview);
 
 router.get("/spot/:spotId", reviewController.showAllReview);
 router.get("/:reviewId", reviewController.showOneReview);
